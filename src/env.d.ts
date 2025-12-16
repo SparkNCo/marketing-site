4599165a57c9b674d7ed070fa9088e7b7024f057
Santiago <santiagonaguero@gmail.com>
2025-12-16 14:36:21 -0300

    forms-astro-supabase-added

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly PUBLIC_SUPABASE_ANON_KEY: string
+  readonly PUBLIC_SUPABASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
