729ba3a6df448ae73ef53d11f033a5c44364149c
Santiago <santiagonaguero@gmail.com>
2026-06-11 18:09:56 -0300

    feat/178 removed log to test dorametrics feature

--- src/components/forms/FeaturesCollectionMobile.tsx
+++ src/components/forms/FeaturesCollectionMobile.tsx
@@ -44,8 +44,6 @@ export function FeaturesCollectionMobile({
   // populate local state when data arrives
   useEffect(() => {
     if (featuresQuery.data) {
-      console.log("test");
-    
       setFeatures(featuresQuery.data);
     }
   }, [featuresQuery.data]);
